89b0f7882985f8c19732ffe80daf3b3446e01104
ofentse <ofentsed1@educbe.ca>
2026-04-07 23:34:11 +0000

    added the const's in movelogic

--- moveLogic.js
+++ moveLogic.js
@@ -26,15 +26,30 @@ export default function move(gameState){
     // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     // gameState.board contains an object representing the game board including its width and height
     // https://docs.battlesnake.com/api/objects/board
+    const boardWidth = gameState.board.width;
+    const boardHeight = gameState.board.height;
+
     
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
+     if (myHead.x === 0)  moveSafety.left = false;
+     if (myHead.x === boardWidth - 1)  moveSafety.right = false;
+     if (myHead.y === 0) moveSafety.down = false;
+     if (myHead.y === boardHeight - 1) moveSafety.up = false;
     
     
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
+
+    const candidates = {
+        up:{ x: myHead.x,  y: myHead.y + 1 },
+        down:{ x: myHead.x,  y: myHead.y - 1 },
+        left:{ x: myHead.x - 1,  y: myHead.y },
+        right:{ x: myHead.x + 1,  y: myHead.y },
+    };
+
     
     // Are there any safe moves left?
     
